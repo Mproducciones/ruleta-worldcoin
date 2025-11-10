33f4a7545b377a3cc4353ae63a1d549b66095693
MProducciones <mentalistaproducciones@gmail.com>
2025-11-10 19:59:00 -0300

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext',  // Mejora compatibilidad con Vercel
+    assetsInlineLimit: 0,  // Carga assets externos
+  },
+  define: {
+    global: 'globalThis',
+  },
+});
